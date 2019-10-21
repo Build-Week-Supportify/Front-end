17c027d1782e38af40652dc6850e72669048df3c
Hunter5555 <hunter.m.allan@gmail.com>
2019-10-21 15:49:12 -0600

    added components files and created basic nav

---
+++ supportify/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <header id="header">
+                <h1 id="logo"><a href="#">Supportify</a></h1>
+                <nav id="nav">
+                    <ul>
+                      
+                    </ul>
+                </nav>
+            </header>
+        );
+    }
+}
+
+export default Nav;
