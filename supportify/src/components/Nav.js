6e5a125a6eeb93d7f4bcf8f53f2bec1e46adaf34
Hunter5555 <hunter.m.allan@gmail.com>
2019-10-23 09:05:03 -0600

    need to start styling

--- supportify/src/components/Nav.js
+++ supportify/src/components/Nav.js
@@ -4,10 +4,11 @@ class Nav extends React.Component {
     render() {
         return (
             <header id="header">
-                <h1 id="logo"><a href="#">Supportify</a></h1>
                 <nav id="nav">
                     <ul>
-                      
+                    <h3 id="logo"><a href="#">Login</a></h3>
+                    <h3 id="logo"><a href="#">Sign up</a></h3>
+                    <h3 id="logo"><a href="#">Account</a></h3>
                     </ul>
                 </nav>
             </header>
