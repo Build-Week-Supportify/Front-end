b5e9b0d6a56c328fc8b6f76985d12c4903f4c5d0
gspayonk <grissobel.castro@gmail.com>
2019-10-24 14:10:09 -0400

    school list and functionality added

--- supportify/src/components/schools/SchoolList.js
+++ supportify/src/components/schools/SchoolList.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import School from './School';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import SchoolForm from "./SchoolForm";
+export default class SchoolList extends Component {
 
-const SchoolList = ({ list }) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+        schools: []
+    };
+}
 
-    console.log('school list', list);
+    componentDidMount() {
+        axios
+        .get("https://bw-supportify.herokuapp.com/schools")
+
+        .then(response => this.setState({ schools: response.data }))
+
+        .catch(error => console.log(error.response));
+
+        this.setState({schools: []});
+    }
+
+  //updates - passes two states - can collect input data from user to use Ajax to upload to database
+    componentDidUpdate() {
+        axios
+        .get("https://bw-supportify.herokuapp.com/schools")
+
+        .then(response => this.setState({ schools: response.data }))
+        .catch(error => console.log(error.response));
+    }
+
+    render() {
+        return (
+        <div >
+        {this.state.schools.map(school=> (
+            <SchoolDetails key={school.id} school={school} />
+        ))}
+        </div>
+        );
+    }
+}
+
+function SchoolDetails({ school }) {
     return (
-    <div className = 'item-container'>
-        {list && list.map(item => <School key={item.id} item={item} />)}
-    </div>
+        <Link to={`/schools/${school.id}`}>
+        <SchoolForm school={school} />
+        </Link>
     );
-};
-
-export default SchoolList;
+}
